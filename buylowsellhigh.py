467d1c5a546bc9c0d11f7fd5d832bb46c4aba931
perchik_air <1919ars@gmail.com>
2021-12-21 20:43:50 +0200

    (air)

--- buylowsellhigh.py
+++ buylowsellhigh.py
@@ -5,7 +5,7 @@ import numpy as np
 # loading the class data from the package pandas_datareader
 from pandas_datareader import data
 
-
+# GETTING THE DATA
 # First day
 start_date = '2017-01-01'
 # Last day
@@ -13,15 +13,39 @@ end_date = '2021-01-01'
 # Call the function DataReader from the class data
 goog_data = data.DataReader('GOOG', 'yahoo', start_date, end_date)
 
+# BUILDING SIGNALS
 goog_data_signal = pd.DataFrame(index=goog_data.index)
-
-# fig = px.line(goog_data, y=['Close', 'Open', 'High', 'Low'])
-# fig.show()
-print(goog_data.head())
-# plt.plot(goog_data['Close'], label='close')
-plt.plot(goog_data['Adj Close'], label='Adj Close')
-# plt.plot(goog_data['Open'], label='Open')
-# plt.plot(goog_data['High'], label='High')
-# plt.plot(goog_data['Low'], label='Low')
+goog_data_signal['price'] = goog_data['Adj Close']
+goog_data_signal['daily_difference'] = goog_data_signal['price'].diff()
+goog_data_signal['signal'] = 0.0
+goog_data_signal['signal'] = np.where(goog_data_signal['daily_difference'] > 0, 1.0, 0.0)
+goog_data_signal['positions'] = goog_data_signal['signal'].diff()
+print(goog_data_signal)
+# print(goog_data_signal['signal'])
+# plt.plot(goog_data_signal['daily_difference'], label='daily_difference')
+plt.plot(goog_data_signal['price'], label='price', color='k', lw=2.)
+plt.plot(goog_data_signal.loc[goog_data_signal.positions == 1.0].index,
+         goog_data_signal.price[goog_data_signal.positions == 1.0], '^', markersize=5, color='g', label='buy')
+plt.plot(goog_data_signal.loc[goog_data_signal.positions == -1.0].index,
+         goog_data_signal.price[goog_data_signal.positions == -1.0], 'v', markersize=5, color='r', label='sell')
 plt.legend()
 plt.show()
+
+# BACKTESTING
+initial_capital = float(1000.0)
+positions = pd.DataFrame(index=goog_data_signal.index).fillna(0.0)
+portfolio = pd.DataFrame(index=goog_data_signal.index).fillna(0.0)
+positions['GOOG'] = goog_data_signal['signal']
+portfolio['positions'] = (positions.multiply(goog_data_signal['price'], axis=0))
+portfolio['cash'] = initial_capital - (positions.diff().multiply(goog_data_signal['price'], axis=0)).cumsum()
+portfolio['total'] = portfolio['positions'] + portfolio['cash']
+
+plt.plot(portfolio['total'], label='total')
+plt.legend()
+plt.show()
+
+
+
+
+
+
