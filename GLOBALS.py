05ae69ee67b84c26882b67b984ab6d2ad4ce059e
perchik_air <1919ars@gmail.com>
2021-12-25 12:14:58 +0200

    (air)

--- GLOBALS.py
+++ GLOBALS.py
@@ -5,6 +5,9 @@ import numpy as np
 from pandas_datareader import data
 import statistics as stats
 import math as math
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_pacf
+from statsmodels.tsa.arima.model import ARIMA
 
 
 # First day
